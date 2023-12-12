a60bb9cacf2c662b24323c184ad8f46df3387186
Hristiyan22 <80088595+Hristiyan22@users.noreply.github.com>
2023-12-12 19:30:59 +0200

    Update UserManager.java

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+class UserManager {
+    public static void mainMenu() {
+       
+    }
+
+    private static void checkAdmin() {
+       
+    }
+
+    private static void checkEmployee() {
+        
+    }
