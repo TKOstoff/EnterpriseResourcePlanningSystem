0173544814f9eddedd01c614eba638ef87cc4a99
Hristiyan22 <80088595+Hristiyan22@users.noreply.github.com>
2023-12-15 01:18:08 +0200

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,78 +1,132 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
-public class Admin extends User{
-
-   private Map<String, Project> projects;
-    private List<Client> clients;
-    private List<Employee> employees;
-    private List<String> registeredEmployees;
-    private boolean isAdmin;
-    private static final String ADMIN_USERNAME = "admin";
-    private static final String ADMIN_PASSWORD = "1234";
+class Admin extends User implements EmployeeManagement, ClientManagement {
+    private List<Client> clients = new ArrayList<>();
+    private Map<String, Employee> employees = new HashMap<>();
+   
 
     public Admin(String username, String password) {
         super(username, password);
-        this.projects = new HashMap<>();
-        this.clients = new ArrayList<>();
-        this.employees = new ArrayList<>();
-        this.registeredEmployees = new ArrayList<>();
-        this.isAdmin = true;
-        super.username = ADMIN_USERNAME;
-        super.password = ADMIN_PASSWORD;
-      
-   private List<Client> clients = new ArrayList<>();
-    private Map<String, Employee> employees = new HashMap<>();
-
+    }
 
     @Override
     void menu() {
-        
+        Scanner scanner = new Scanner(System.in);
+        int adminChoice;
+
+        while (true) {
+            System.out.println(" ");
+            System.out.println("Меню за администратор:");
+            System.out.println("1. Въвеждане на нови клиенти");
+            System.out.println("2. Регистрация на нови служители");
+            System.out.println("3. Гледане на статистика");
+            System.out.println("4. Връщане");
+            System.out.println("0. Излизане от програмата.");
+
+            adminChoice = scanner.nextInt();
+
+            switch (adminChoice) {
+                case 1:
+                    newClient();
+                    break;
+
+                case 2:
+                    System.out.println(" ");
+                    System.out.print("Потребителско име: ");
+                    String employeeUsername = scanner.next();
+                    System.out.print("Парола: ");
+                    String employeePassword = scanner.next();
+
+                    registerEmployee(employeeUsername, employeePassword);
+
+                    break;
+
+                case 3:
+                    showStatisticsMenu();
+                    break;
+                case 4:
+                    System.out.println("Връщане...");
+                    System.out.println(" ");
+                    UserManager.mainMenu();
+                    return;
+                case 0:
+                    System.out.println("Излизане от програмата...");
+                    break;
+                default:
+                    System.out.println("Грешен избор.");
+            }
+        }
     }
 
-
     @Override
-    public void addClient(String name, String projectName, String contractExpirationDate) {
-        System.out.println("Име на клиента: " + name);
-        System.out.println("Име на проекта: " + projectName);
-        System.out.println("Дата на изтичане на договора: " + contractExpirationDate);
+    public void addClient(String name, String projectName, Date contractExpirationDate) {
+      
+    }
 
-        clients.add(new Client(name, projectName, contractExpirationDate));
+    private void saveClientToFile(String name, String projectName, Date contractExpirationDate) {
+       
+    }
 
-        System.out.println("Клиента с име " + name + " и проект '" + projectName +
-                "' с изтичане на договора на '" + contractExpirationDate + "' е успешно добавен.");
+    private void newClient() {
+       
     }
 
     @Override
     public void registerEmployee(String username, String password) {
         if (isEmployeeUsernameUnique(username)) {
-            System.out.println(" ");
-            System.out.println("Потребителско име: " + username);
-            System.out.println("Парола: " + password);
-
             employees.put(username, new Employee(username, password));
-
             System.out.println(username + " е успешно регистриран като нов служител.");
+
+            try (FileWriter writer = new FileWriter("employees.txt", true)) {
+                writer.write(username + "," + password + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         } else {
-            System.out.println("Потребителското име " + username + " вече съществува. Моля, изберете  друго.");
+            System.out.println("Потребителското име " + username + " вече съществува. Моля, изберете друго.");
         }
     }
+
     private boolean isEmployeeUsernameUnique(String username) {
         return !employees.containsKey(username);
     }
-  
 
-public void showMainMenu{
-
+    public void showMainMenu() {
     }
+
     private void showStatisticsMenu() {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Статистика:");
+            System.out.println("1. Търсене по име на служител");
+            System.out.println("2. Търсене по номер на седмица");
+            System.out.println("3. Назад");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    searchByName();
+                    break;
+
+                case 2:
+                    searchByWeek();
+                    break;
+
+                case 3:
+                    return;
+
+                default:
+                    System.out.println("Грешен избор.");
+            }
+        }
     }
 
     private void searchByName() {
-       
+        
     }
 
     private void searchByWeek() {
