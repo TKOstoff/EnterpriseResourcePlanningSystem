686dca8ae20a1307a3a730ec58ed3e650c984509
Hristiyan22 <80088595+Hristiyan22@users.noreply.github.com>
2023-12-16 19:30:11 +0200

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,32 +1,18 @@
 abstract class User {
-    private  String username;
-    private   String password;
+    private String username;
+    private String password;
 
-    public User() {
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
-
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = this.username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-
-
-    protected abstract void menu();
+    abstract void menu();
 }
